221c6fe9ab13b4e3e1608582810e50d5c7ddd938
ingalls <nicholas.ingalls@gmail.com>
2014-03-04 10:49:18 -0500

    Flesh out search & crag list features

--- app/services/DummyDataService.js
+++ app/services/DummyDataService.js
@@ -7,13 +7,14 @@ app.service("DummyDataService", function(UtilService){
       for ( var i = 0; i < length; i++) {
         var crag = {};
         crag.id = UtilService.randomNumber(0,20000);
-        crag.name = "Dummy crag name";
+        crag.name = "Creature Feature at Phantasia";
         crag.thumbnail_image_url = "http://lorempixel.com/100/100/nature/" + UtilService.randomNumber(0,10).toString();
         crag.description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe, delectus, doloremque necessitatibus velit deleniti tempore ratione distinctio dolor sequi aliquam.";
         crag.number_of_walls = UtilService.randomNumber(3, 18);
         crag.number_of_routes = UtilService.randomNumber(45,350);
         crag.lon = UtilService.randomLatLon();
         crag.lat = UtilService.randomLatLon();
+        crag.rating = UtilService.randomNumber(0,5);
 
         crags.push(crag);
       }
